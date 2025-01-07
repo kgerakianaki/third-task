9b613ae6d884a5c453bf3d4e5273233b75fdd500
kgerakianaki <mkg@entersoft.gr>
2025-01-07 16:45:58 +0200

    form start

--- third/src/app/components/form/form.component.ts
+++ third/src/app/components/form/form.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
+import { TranslateModule } from "@ngx-translate/core";
+import { SKTelInputModule, SKTelInputOutput } from "sk-tel-input";
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss'],
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.scss"],
+  imports: [TranslateModule, IonicModule, SKTelInputModule]
 })
-export class FormComponent  implements OnInit {
-
-  constructor() { }
+export class FormComponent implements OnInit {
+  emailPattern: string = "^[a-zA-Z0-9._]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+  constructor() {}
 
   ngOnInit() {}
 
+  // This method returns the value of a selected control of the form
+  getFormControlValue(givenControlTag: string): any {
+    //return this.testForm.get(givenControlTag).value;
+  }
+
+  // This method is handling the output of the sk tel input
+  handleSkTelInputContents(skTelOutput: SKTelInputOutput) {
+    //this.helper.consoleHandler("SK Tel Input Output: ", skTelOutput);
+
+    // Proper setup the content and the form fields
+    //this.testForm
+   //   .get("prefix")
+   //   .setValue(`+${skTelOutput.selectedCountryCode.dialCode}`);
+    //this.testForm.get("phone").setValue(`${skTelOutput.telInputContents}`);
+  }
 }
--- third/src/app/components/swiper/swiper.component.ts
+++ third/src/app/components/swiper/swiper.component.ts
@@ -14,6 +14,6 @@ export class SwiperComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-   
+ 
   }
 }
